14066a31ddb6223d68f41e5f03d5458448d0ee43
Ray <zrztt@outlook.com>
2020-06-23 22:40:45 +0800

    chore(*): Initialize file
    + add main.js
    + add instructions in README

---
+++ main.js
@@ -0,0 +1,14 @@
+(function () {
+	document.getElementById("btn-readmore-zk").click();
+	$("#side").remove();
+	$("#comment_title, #comment_list, #comment_bar, #comment_form, .announce, #ad_cen, #ad_bot").remove();
+	$(".nav_top_2011, #header, #navigator").remove();
+	$(".p4course_target, .comment-box, .recommend-box, #csdn-toolbar, #tool-box").remove();
+	$("aside").remove();
+	$(".tool-box").remove();
+	$(".left-toolbox").remove();
+	$(".template-box").remove();
+	$(".bottom-pub-footer").remove();
+
+	window.print();
+})();
