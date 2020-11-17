1ed7e42db560bff975101e0f6e6cf02bcb6825d1
RayK <zrztt@outlook.com>
2020-11-17 14:36:12 +0800

    feat: 适配新版CSDN
    修改元素remove方式，使用siblings().

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-(function () {
-	document.getElementById("btn-readmore-zk").click();
-	$("#side").remove();
-	$("#comment_title, #comment_list, #comment_bar, #comment_form, .announce, #ad_cen, #ad_bot").remove();
-	$(".nav_top_2011, #header, #navigator").remove();
-	$(".p4course_target, .comment-box, .recommend-box, #csdn-toolbar, #tool-box").remove();
-	$("aside").remove();
-	$(".tool-box").remove();
-	$(".left-toolbox").remove();
-	$(".template-box").remove();
-	$("#pub-footerall").remove();
-	$("main").css('display','content');
+(function() {
+    $("head").remove()
+    $(".main_father").siblings().remove()
+    $("#mainBox").siblings().remove()
+    $("aside").remove()
+    $(".blog-content-box").siblings().remove()
+    $(".article-title-box").siblings().remove()
 
-	window.print();
+    $("main").css('display', 'content')
+
+    window.print();
 })();
